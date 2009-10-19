16094c22f2779698b19d4e9b6068d238a837f7af
Chaim Kirby <ckirby@ckirby-desktop.(none)>
2009-10-19 15:48:29 -0400

    Updated to work with our profile columns

--- db/migrate/001_create_staff_profiles.rb
+++ db/migrate/001_create_staff_profiles.rb
@@ -3,11 +3,21 @@ class CreateStaffProfiles < ActiveRecord::Migration
     create_table :staff_profiles do |t|
       t.string   :first_name
       t.string   :last_name
+      t.string   :degree
       t.string   :title
+      t.integer   :position_type,  :default => 0, :null => false
       t.string   :email
+      t.string   :phone
+      t.string   :fax
+      t.string   :website
+      t.text     :address
       t.integer  :status_id,  :default => 1, :null => false
       t.string   :filter_id
       t.text     :biography
+      t.text     :affiliations
+      t.text     :research
+      t.text     :clinical_interests
+      t.text     :publications
       t.string   :photo_file_name
       t.string   :photo_content_type
       t.integer  :photo_file_size
