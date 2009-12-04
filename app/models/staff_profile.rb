5aa3d9f266bf44801c2a40a95cd13183ffd2b11c
Chaim Kirby <ckirby@ckirby-desktop.(none)>
2009-12-04 14:38:51 -0500

    Fixes:

--- app/models/staff_profile.rb
+++ app/models/staff_profile.rb
@@ -67,30 +67,6 @@ class StaffProfile < ActiveRecord::Base
     update_attribute(:session_token, nil)
   end
 
-  private
-
-    def validate_length_of_password?
-      new_record? or not password.to_s.empty?
-    end
-
-    before_create :encrypt_password
-    def encrypt_password
-      self.salt = Digest::SHA1.hexdigest("--#{Time.now}--#{login}--sweet harmonious biscuits--")
-      self.password = sha1(password)
-    end
-
-    before_update :encrypt_password_unless_empty_or_unchanged
-    def encrypt_password_unless_empty_or_unchanged
-      user = self.class.find(self.id)
-      case password
-      when ''
-        self.password = user.password
-      when user.password
-      else
-        encrypt_password
-      end
-    end
-
   def status
     Status.find(self.status_id)
   end
@@ -119,8 +95,32 @@ class StaffProfile < ActiveRecord::Base
       self.biography
     end
   end
-  
+
   def position_name
     position.name
   end
+  
+  private
+
+    def validate_length_of_password?
+      new_record? or not password.to_s.empty?
+    end
+
+    before_create :encrypt_password
+    def encrypt_password
+      self.salt = Digest::SHA1.hexdigest("--#{Time.now}--#{login}--sweet harmonious biscuits--")
+      self.password = sha1(password)
+    end
+
+    before_update :encrypt_password_unless_empty_or_unchanged
+    def encrypt_password_unless_empty_or_unchanged
+      user = self.class.find(self.id)
+      case password
+      when ''
+        self.password = user.password
+      when user.password
+      else
+        encrypt_password
+      end
+    end
 end
