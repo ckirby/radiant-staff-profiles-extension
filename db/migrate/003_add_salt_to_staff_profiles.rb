b71bd1f12a69dae5257ac08b0cb902a0f0a66aac
Chaim Kirby <ckirby@ckirby-desktop.(none)>
2009-12-04 14:56:55 -0500

    Added Salt field to staff_profiles table

---
+++ db/migrate/003_add_salt_to_staff_profiles.rb
@@ -0,0 +1,11 @@
+class AddSaltToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :staff_profiles, :salt, :string
+    StaffProfiles.reset_column_information
+    StaffProfiles.update_all :salt => "sweet harmonious biscuits"
+  end
+
+  def self.down
+    remove_column :staff_profiles, :salt
+  end
+end
