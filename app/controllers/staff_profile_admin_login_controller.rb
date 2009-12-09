42f136176be42d9c4f3db9483a74a538d7c06603
Chaim Kirby <ckirby@ckirby-desktop.(none)>
2009-12-09 13:13:07 -0500

    Test Login by just doing a redirect

--- app/controllers/staff_profile_admin_login_controller.rb
+++ app/controllers/staff_profile_admin_login_controller.rb
@@ -3,18 +3,19 @@ class StaffProfileAdminLoginController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def login
-    if request.post?
-      login = params[:username]
-      password = params[:password]
+    redirect_to "http://boston.com"
+#    if request.post?
+#      login = params[:username]
+#      password = params[:password]
 #     announce_invalid_user unless self.current_user = User.authenticate(login, password)
-    end
-    if current_user
-      if params[:remember_me]
-        current_user.remember_me
-        set_session_cookie
-      end
-      redirect_to (session[:return_to] || staff_profile_admin_url)
-      session[:return_to] = nil
-    end
+#    end
+#    if current_user
+#      if params[:remember_me]
+#        current_user.remember_me
+#        set_session_cookie
+#      end
+#      redirect_to (session[:return_to] || staff_profile_admin_url)
+#      session[:return_to] = nil
+#    end
   end
 end
