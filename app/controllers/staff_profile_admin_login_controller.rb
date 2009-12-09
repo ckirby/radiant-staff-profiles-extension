b5ad8be2f3435b3d9d7dbf56a5594956d905020e
Chaim Kirby <ckirby@ckirby-desktop.(none)>
2009-12-09 13:18:45 -0500

    Test that credentials are POSTing

--- app/controllers/staff_profile_admin_login_controller.rb
+++ app/controllers/staff_profile_admin_login_controller.rb
@@ -3,19 +3,13 @@ class StaffProfileAdminLoginController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def login
-    redirect_to "http://boston.com"
-#    if request.post?
-#      login = params[:username]
-#      password = params[:password]
+    if request.post?
+      login = params[:username]
+      password = params[:password]
+      puts "Login: #{$login} #{$password}"
 #     announce_invalid_user unless self.current_user = User.authenticate(login, password)
-#    end
-#    if current_user
-#      if params[:remember_me]
-#        current_user.remember_me
-#        set_session_cookie
-#      end
-#      redirect_to (session[:return_to] || staff_profile_admin_url)
-#      session[:return_to] = nil
-#    end
+    else
+      puts "No Login Info"
+    end
   end
 end
