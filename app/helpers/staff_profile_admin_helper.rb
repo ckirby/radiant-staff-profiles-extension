b60fa260a5ea4d4dbbe439311b294150ed517c55
Chaim Kirby <ckirby@ckirby-desktop.(none)>
2009-12-15 17:08:31 -0500

    Moved file to correct locationy

--- app/helpers/staff_profile_admin_helper.rb
+++ app/helpers/staff_profile_admin_helper.rb
@@ -1,6 +1,6 @@
-module StafffProfileAdmin::ProfilesHelper
+module StaffProfileAdmin::StaffProfileAdminHelper
   def form_for_staff_profile_admin(profile, html_options = {}, &block)
-    url  staff_profile_admin_url(profile)
+    url = staff_profile_admin_url(profile)
     html_options[:multipart] = true
     html_options[:method] = :put 
     form_for(:profile, @profile, :url => url, :html => html_options, &block)
